4fca873899372ceb0d7b6947d868be8293a4b86b
VankoProject <ivan.green69@gmail.com>
2023-08-20 13:20:20 +0300

    create architecture structure

---
+++ app/src/main/java/com/kliachenko/gitvoyager/presentation/UserViewModel.kt
@@ -0,0 +1,8 @@
+package com.kliachenko.gitvoyager.presentation
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+
+@HiltViewModel
+class UserViewModel: ViewModel() {
+}
--- app/src/main/java/com/kliachenko/gitvoyager/ui/theme/Color.kt
+++ app/src/main/java/com/kliachenko/gitvoyager/ui/theme/Color.kt
@@ -2,7 +2,11 @@ package com.kliachenko.gitvoyager.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+val Blue = Color(0xff4078c0)
+val Green = Color(0xff6cc644)
+val Red = Color(0xffbd2c00)
+val Orange = Color(0xFFc9510c)
+val Violet = Color(0xFF6e5494)
+val White = Color(0xFFfafafa)
+val LightGrey = Color(0xFFf5f5f5)
+val DarkGrey = Color(0xFF333333)
