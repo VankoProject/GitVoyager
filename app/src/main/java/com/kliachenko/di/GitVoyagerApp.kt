5a4a6b75ebc221cfe8b4ebc7aecd5a816a6f7e0b
VankoProject <ivan.green69@gmail.com>
2023-08-20 19:25:14 +0300

    create domain layer with RepositoryImpl method

--- app/src/main/java/com/kliachenko/di/GitVoyagerApp.kt
+++ app/src/main/java/com/kliachenko/di/GitVoyagerApp.kt
@@ -1,4 +1,4 @@
-package com.kliachenko
+package com.kliachenko.di
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
--- app/src/main/java/com/kliachenko/gitvoyager/presentation/UserViewModel.kt
+++ app/src/main/java/com/kliachenko/gitvoyager/presentation/UserViewModel.kt
@@ -2,7 +2,8 @@ package com.kliachenko.gitvoyager.presentation
 
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
 @HiltViewModel
-class UserViewModel: ViewModel() {
+class UserViewModel @Inject constructor(): ViewModel() {
 }
